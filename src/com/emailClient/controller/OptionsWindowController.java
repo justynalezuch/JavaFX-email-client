4cdf36eeec305148515d41ffcccc5d5edc3c4528
justynalezuch <justynalezuch@gmail.com>
2021-07-28 13:22:51 +0200

    Add enum ColorTheme values to choice box

--- src/com/emailClient/controller/OptionsWindowController.java
+++ src/com/emailClient/controller/OptionsWindowController.java
@@ -1,18 +1,24 @@
 package com.emailClient.controller;
 
 import com.emailClient.EmailManager;
+import com.emailClient.view.ColorTheme;
 import com.emailClient.view.ViewFactory;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 
-public class OptionsWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OptionsWindowController extends BaseController implements Initializable {
 
     @FXML
     private Slider fontSizePicker;
 
     @FXML
-    private ChoiceBox<?> themePicker;
+    private ChoiceBox<ColorTheme> themePicker;
 
     public OptionsWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -26,5 +32,16 @@ public class OptionsWindowController extends BaseController {
     @FXML
     void cancelButtonAction() {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setUpThemePicker();
+
+    }
+
+    private void setUpThemePicker() {
+        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
+        themePicker.setValue(viewFactory.getColorTheme());
     }
 }
